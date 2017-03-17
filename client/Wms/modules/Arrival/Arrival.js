9a84241ae08962b34f7fb46a8ff317b5bfc0d269
Vladimir Logachev <doit@keemail.me>
2017-03-17 21:16:20 +0700

    ListView updated a bit

--- client/Wms/modules/Arrival/Arrival.js
+++ client/Wms/modules/Arrival/Arrival.js
@@ -5,11 +5,20 @@ import ListView from './../../components/ListView/ListView';
 import DocumentContainer from './../../components/DocumentContainer/DocumentContainer';
 import Navigation from './../../components/Navigation/Navigation';
 
+const documentsList = [
+  { name: '17.01.17 09:12', _id: '3567357' },
+  { name: '17.01.17 10:12', _id: '35675367' },
+  { name: '17.01.17 11:12', _id: '356735672' },
+  { name: '17.01.17 12:12', _id: '54674567' },
+  { name: '17.01.17 13:12', _id: '456457' },
+  { name: '17.01.17 14:12', _id: '45745' },
+];
+
 const Arrival = () => {
   return (
     <div>
       <Helmet title="Arrival" />
-      <ListView />
+      <ListView list={documentsList} urlPrefix="arrival" documentType="Arrival act" header="Arrival acts" />
       <DocumentContainer />
       <Navigation />
     </div>
--- client/Wms/modules/Products/Products.js
+++ client/Wms/modules/Products/Products.js
@@ -5,11 +5,20 @@ import ListView from './../../components/ListView/ListView';
 import DocumentContainer from './../../components/DocumentContainer/DocumentContainer';
 import Navigation from './../../components/Navigation/Navigation';
 
+const productsList = [
+  { name: '17.01.17 09:12', _id: 'lk35jl3k' },
+  { name: '17.01.17 10:12', _id: '8fg78fg86' },
+  { name: '17.01.17 11:12', _id: 's0fgh4' },
+  { name: '17.01.17 12:12', _id: 'klnl3k46' },
+  { name: '17.01.17 13:12', _id: 'sd8g7sdf86' },
+  { name: '17.01.17 14:12', _id: 'lk109dfg' },
+];
+
 const Products = () => {
   return (
     <div>
       <Helmet title="Products" />
-      <ListView />
+      <ListView list={productsList} urlPrefix="product" documentType="Product" />
       <DocumentContainer />
       <Navigation />
     </div>
