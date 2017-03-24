5a7e9a1df30a522c28ab14f2f4f19f7e98ef3dd9
mkarabashev <mkarabashev@gmail.com>
2017-03-25 00:46:44 +0200

    removed an unused file

--- client/Wms/HOC/createTable/index.js
+++ client/Wms/HOC/createTable/index.js
@@ -1,6 +1,5 @@
 import createTable from './createTable';
 export createCell from './createCell';
-export TableHeading from './TableHeading';
 export createRowType from './createRowType';
 
 export default createTable;
