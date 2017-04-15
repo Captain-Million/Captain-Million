9c0de64899ac1f6e31001ef259301e3d9b857a2e
Vladimir Logachev <doit@keemail.me>
2017-04-15 22:52:44 +0700

    landing more

--- client/Landing/components/Disclaimer/Disclaimer.js
+++ client/Landing/components/Disclaimer/Disclaimer.js
@@ -4,19 +4,18 @@ import { Link } from 'react-router-dom';
 import styles from './Disclaimer.css';
 
 const Disclaimer = () => {
-
   return (
     <div className={styles.disclaimer}>
-        <div>
-          <h3>Live dangerously!</h3>
-          <p>We offer our&nbsp;software <strong>as&nbsp;is</strong>,&nbsp;for&nbsp;free.</p>
-          <p className={styles.yellow}>We do&nbsp;not promise&nbsp;you neither&nbsp;uninterrupted&nbsp;operation,
-          <br />nor&nbsp;technical support or&nbsp;even&nbsp;backups.</p>
-          <p>The less you&nbsp;get, the&nbsp;less&nbsp;you&nbsp;can worry&nbsp;about,&nbsp;isn’t&nbsp;it&nbsp;so?</p>
-          <p>Join <strong>0</strong>&nbsp;companies, already using WMS-proj. Today,&nbsp;for&nbsp;Free!</p>
-          <Link to="/wms" className={styles.disclaimerBtn}>Start using</Link>
-        </div>
+      <div>
+        <h3>Live dangerously!</h3>
+        <p>We offer our&nbsp;software <strong>as&nbsp;is</strong>,&nbsp;for&nbsp;free.</p>
+        <p className={styles.yellow}>We do&nbsp;not promise&nbsp;you neither&nbsp;uninterrupted&nbsp;operation,
+        <br />nor&nbsp;technical support or&nbsp;even&nbsp;backups.</p>
+        <p>The less you&nbsp;get, the&nbsp;less&nbsp;you&nbsp;can worry&nbsp;about,&nbsp;isn’t&nbsp;it&nbsp;so?</p>
+        <p>Join <strong>0</strong>&nbsp;companies, already using WMS-proj. Today,&nbsp;for&nbsp;Free!</p>
+        <Link to="/wms" className={styles.disclaimerBtn}>Start using</Link>
       </div>
+    </div>
   );
 };
 
