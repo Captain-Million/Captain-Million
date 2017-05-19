c11519ab8e1b47129c9bcee63bf2ed070709dbee
ksmai <kasingmai@gmail.com>
2017-05-19 13:49:18 +0800

    Fix DocumentHeader tests

--- client/Wms/components/DocumentHeader/DocumentHeader.spec.js
+++ client/Wms/components/DocumentHeader/DocumentHeader.spec.js
@@ -20,7 +20,7 @@ test('should be able to add an item type', (t) => {
     itemType: 'item',
   };
   const wrapper = shallow(<DocumentHeader {...props} />);
-  const itemSpan = wrapper.find('div').find('span');
+  const itemSpan = wrapper.find('div').find('span').at(0);
   t.true(itemSpan.hasClass(styles.type));
   t.is(itemSpan.text(), props.itemType);
 });
@@ -31,7 +31,7 @@ test('should be able to add a name', (t) => {
     itemType: 'item',
   };
   const wrapper = shallow(<DocumentHeader {...props} />);
-  const nameSpan = wrapper.find('div').find('span');
+  const nameSpan = wrapper.find('div').find('span').at(1);
   t.true(nameSpan.hasClass(styles.name));
   t.is(nameSpan.text(), ` ${props.name}`);
 });
