e19f3e5c38d0545129f8cf8c3eb209ee577f6f5d
Vladimir Logachev <doit@keemail.me>
2017-05-19 12:26:07 +0700

    removed all warnings, but got 1 error, need help

--- client/Wms/components/DocumentHeader/DocumentHeader.spec.js
+++ client/Wms/components/DocumentHeader/DocumentHeader.spec.js
@@ -5,13 +5,20 @@ import DocumentHeader from './DocumentHeader';
 import styles from './DocumentHeader.css';
 
 test('should have a document-header class', (t) => {
-  const wrapper = shallow(<DocumentHeader />);
+  const props = {
+    name: 'name',
+    itemType: 'item',
+  };
+  const wrapper = shallow(<DocumentHeader {...props} />);
   t.is(wrapper.type(), 'div');
   t.true(wrapper.hasClass(styles.documentHeader));
 });
 
 test('should be able to add an item type', (t) => {
-  const props = { itemType: 'item' };
+  const props = {
+    name: 'name',
+    itemType: 'item',
+  };
   const wrapper = shallow(<DocumentHeader {...props} />);
   const itemSpan = wrapper.find('div').find('span');
   t.true(itemSpan.hasClass(styles.type));
@@ -19,10 +26,12 @@ test('should be able to add an item type', (t) => {
 });
 
 test('should be able to add a name', (t) => {
-  const props = { name: 'name' };
+  const props = {
+    name: 'name',
+    itemType: 'item',
+  };
   const wrapper = shallow(<DocumentHeader {...props} />);
   const nameSpan = wrapper.find('div').find('span');
-
   t.true(nameSpan.hasClass(styles.name));
   t.is(nameSpan.text(), ` ${props.name}`);
 });
